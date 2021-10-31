1a67b86a01385cda6dc4290c3b51c82aadede674
Preeti Maurya <preetimaurya3344@gmail.com>
2021-10-31 19:55:39 +0530

    add changes

--- src/dataCenter.js
+++ src/dataCenter.js
@@ -129,7 +129,103 @@ const items = [
         }
         
 
+    },
+
+
+
+    {
+        name: 'OPPO A74 5G',
+        category: 'mobile',
+        price:15999,
+        totalPrice:20999,
+        desc: [
+                '6.49" Inch 16.5cm FHD+ Punch-hole Display with 2400x1080 pixels. Larger screen to body ratio of 90.5%.Side Fingerprint Sensor',
+                'Qualcomm Snapdragon 480 5G GPU 619 at 650 MHz Support 5G sim Powerful 2 GHz Octa-core processor, support LPDDR4X memory and latest UFS 2.1 gear 3 storage',
+                '5000 mAh lithium polymer battery',
+                '48MP Quad Camera 48MP Main + 2MP Macro + 2MP Depth Lens 8MP Front Camera',
+                'Memory, Storage & SIM: 6GB RAM 128GB internal memory expandable up to 256GB Dual SIM nano+nano dual-standby 5G+5G. Color OS 11.1 based on Android v11.0 operating system',
+                'Connector type: USB Type C',
+                'Display type: LCD'
+        ],
+        quantity:50,
+        rating: 3,
+        image:'mobile3.jpg',
+        specification: {
+                        'OS':	'‎Android 11',
+                        'RAM':	'‎6 GB',
+                        'Product Dimensions':	'‎16.3 x 7.5 x 0.8 cm; 188 Grams',
+                        'Batteries':	'‎1 Lithium Polymer batteries required. (included)',
+                        'Item model number':	'‎CPH2263',
+                        'Connectivity technologies':	'‎WiFi Bluetooth',
+                        'GPS':	'‎True',
+                        'Special features':	'‎Gamer mode, Dual SIM, Fingerprint Sensor, Bigger Battery, Smartphone',
+                        'Display technology':	'‎AMOLED',
+                        'Other display features':	'‎Wireless',
+                        'Device interface ':' primary	‎Touchscreen',
+                        'Other camera features':	'‎Triple Rear Camera (48 MP + 2 MP + 2 MP) | 8 MP Front Camera',
+                        'Audio Jack':	'‎3.5 mm',
+                        'Form factor':	'‎Bar',
+                        'Colour':'Fantastic Purple',
+                        'Battery Power Rating':	'‎5000',
+                        'Phone Talk Time':	'‎45 Hours',
+                        'Phone Standby Time (with data)':	'‎475 Hours',
+                        'Whats in the box':	'‎Phone,USB cable,Charger,SIM ejector tool,Protective case,Quick start guide,Safety guide',
+                        'Manufacturer':	'‎OPPO',
+                        'Country of Origin':	'‎India',
+                        'Item Weight':	'‎188 g',
+        }
+        
+
+    },
+
+    {
+        name: 'OPPO A74 5G',
+        category: 'mobile',
+        price:15999,
+        totalPrice:20999,
+        desc: [
+                '6.49" Inch 16.5cm FHD+ Punch-hole Display with 2400x1080 pixels. Larger screen to body ratio of 90.5%.Side Fingerprint Sensor',
+                'Qualcomm Snapdragon 480 5G GPU 619 at 650 MHz Support 5G sim Powerful 2 GHz Octa-core processor, support LPDDR4X memory and latest UFS 2.1 gear 3 storage',
+                '5000 mAh lithium polymer battery',
+                '48MP Quad Camera 48MP Main + 2MP Macro + 2MP Depth Lens 8MP Front Camera',
+                'Memory, Storage & SIM: 6GB RAM 128GB internal memory expandable up to 256GB Dual SIM nano+nano dual-standby 5G+5G. Color OS 11.1 based on Android v11.0 operating system',
+                'Connector type: USB Type C',
+                'Display type: LCD'
+        ],
+        quantity:50,
+        rating: 3,
+        image:'mobile3.jpg',
+        specification: {
+                        'OS':	'‎Android 11',
+                        'RAM':	'‎6 GB',
+                        'Product Dimensions':	'‎16.3 x 7.5 x 0.8 cm; 188 Grams',
+                        'Batteries':	'‎1 Lithium Polymer batteries required. (included)',
+                        'Item model number':	'‎CPH2263',
+                        'Connectivity technologies':	'‎WiFi Bluetooth',
+                        'GPS':	'‎True',
+                        'Special features':	'‎Gamer mode, Dual SIM, Fingerprint Sensor, Bigger Battery, Smartphone',
+                        'Display technology':	'‎AMOLED',
+                        'Other display features':	'‎Wireless',
+                        'Device interface ':' primary	‎Touchscreen',
+                        'Other camera features':	'‎Triple Rear Camera (48 MP + 2 MP + 2 MP) | 8 MP Front Camera',
+                        'Audio Jack':	'‎3.5 mm',
+                        'Form factor':	'‎Bar',
+                        'Colour':'Fantastic Purple',
+                        'Battery Power Rating':	'‎5000',
+                        'Phone Talk Time':	'‎45 Hours',
+                        'Phone Standby Time (with data)':	'‎475 Hours',
+                        'Whats in the box':	'‎Phone,USB cable,Charger,SIM ejector tool,Protective case,Quick start guide,Safety guide',
+                        'Manufacturer':	'‎OPPO',
+                        'Country of Origin':	'‎India',
+                        'Item Weight':	'‎188 g',
+        }
+        
+
     }
+
+
+
+
 ];
 const categories = ['Home','Mobile', 'Laptop','Cloth','Camera','Book'];
 const NavbarItem = [...categories, 'Login', 'Cart'];
