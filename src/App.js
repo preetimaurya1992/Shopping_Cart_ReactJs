ad8eb6d15f27413bff34db05e94f9141e92792c6
preeti_maurya <preetimaurya3344@gmail.com>
2021-11-07 11:32:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import './App.css';
 import React, { useState } from 'react';
 import Navbar from './navbar/Navbar';
 import ItemBody from './card/ItemBody';
+//import MyLogin from './form/LoginForm';
+import styles from './form/login.module.css';
 
 function App() {
   const [category,setCategory]=useState('Home');
   return (
     <div className="App">
       <Navbar setCategory={setCategory} category={category}/>
-      <ItemBody setCategory={setCategory} category={category}/>
+      {
+        category === 'Login' ? (<div>
+          <div className={styles.formContainer}>
+<h1 className={styles.login}>Login Page</h1>
+  <form className={styles.formMain}>
+    <div className="red">
+  
+    <input type="email" name="email" placeholder="Email" className={styles.inputs}/><br/>
+
+    <input type="password" name="passwoed"  /><br/>
+    <button className={styles.button}>Login</button>
+    </div>
+  </form>
+</div>
+
+          </div>) : <ItemBody setCategory={setCategory} category={category}/>
+      }
     </div>
   );
 }
